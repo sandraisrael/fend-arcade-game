a4f3103d3bb95c0817787ef1a22fe177016c83f7
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2018-01-07 19:58:47 +0100

    update enemy update method

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+
     this.x = x;
     this.y = y;
     this.location = ( x, y);
     this.speed = speed;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies, this uses a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -16,6 +15,8 @@ var Enemy = function(x, y, speed) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
+
+    this.x += this.speed * dt
     // which will ensure the game runs at the same speed for
     // all computers.
 };
